501a06f19467b9bc9581ec080306c46fca466001
pashishsharma385 <53710102+pashishsharma385@users.noreply.github.com>
2020-02-19 15:21:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,22 @@
 // 1.2 Print `"The driver's name is XXXX"`.
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
-
+var Prograd1 = "Ashish";
+var Prograd2 = "Swapnil";
+console.log("The navigator's name is "+Prograd2);
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
+var x =Prograd1.length;
+var y = Prograd2.length;
+if(x>y){
+console.log("The driver has the longest name "+x);
+}
+else{
+  console.log("The Navigator has the longest name, it has "+y" characters");
+}
 
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the number of name, and also print the vowel letters along with the vowel characters. or
@@ -19,20 +29,59 @@
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
+var k = 0, p = 0;
+for (var i = 0; i < Prograd1.length; i++)
+    if(Prograd1.charCodeAt(i)<97){
+        p = p+1;
+    }
+  else{
+        k = k+1;}
+console.log("UpperCase: " + p)
+console.log("LowerCase: " + k)
+
+
 
 
 // Progression 3: Control Statements - 2
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
+if(i == Prograd1.length-1){
+        break;
+    }else{
+        res = res + " ";
+
+
+
 
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+var splting = Prograd2.split("");
+var rev = spliting.reverse();
+var final = rev.join("");
+console.log(final)
+
+
 
 // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
 // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
+var con = ""
+undefined
+ada = ada.concat(Prograd1, " ", Prograd2)
+var a = ada.split(" ")
+var resu = "";
+for (i = a.length-1; i>=0; i--){
+    ru = ru + a[i];
+    if(i == 0){
+        break;
+    }else{
+        ru = ru + " ";}
+}
+console.log(ru)
+
 
 // 3.3 Depending on the lexicographic order of the strings, print:
 // - The driver's name goes first.
 // - Yo, the navigator goes first definitely.
 // - What?! You both have the same name?
+     for(var i =0 )
 
 // Bonus Time!
 // Bonus 1:
